06e1b961f683d0ea7727318a46210e6172fc4604
Juan <j.tejeda46@yahoo.com>
2023-12-28 17:32:53 -0600

    Leetcode 412. First Solution

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -22,5 +22,15 @@
 
 def fizzbuzz(n):
     output = []
-    for i in range(n):
-        pass
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            output.append('FizzBuzz')
+        elif i % 3 == 0:
+            output.append('Fizz')
+        elif i % 5 == 0:
+            output.append('Buzz')
+        else:
+            output.append(f'{i}')
+    return output
+
+print(fizzbuzz(5))
