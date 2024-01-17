64674a8e0f0d3257d88a809ca8f4bbb44e16edea
Juan <j.tejeda46@yahoo.com>
2024-01-16 19:55:12 -0600

    Leetcode 448

--- findAllDisappeared.py
+++ findAllDisappeared.py
@@ -26,4 +26,41 @@ def findAllDisappeared(nums):
 
 
 nums = [4,3,2,7,8,2,3,1]
-print(findAllDisappeared(nums))
+# print(findAllDisappeared(nums))
+
+def findDisappearedN(nums):
+    hashset = {}
+    output = []
+    for i in nums:
+        hashset[i] = hashset.get(i, 0) + 1
+    for i in range(1, len(nums) + 1):
+        if i not in hashset:
+            output.append(i)
+    
+    return output
+
+# print(findDisappearedN(nums))
+
+
+def findAllDisappeared2(nums):
+    for n in nums:
+        i = abs(n) - 1
+        nums[i] = -1 * abs(nums[i])
+    
+    res = []
+    for i, n in enumerate(nums):
+        if n > 0:
+            res.append(i+1)
+    return res
+
+print(findAllDisappeared2([1,2,4,4,5]))
+
+
+def findDisappearedNumbersBF(nums):
+    output = []
+    for i in range(1, len(nums) + 1):
+        if i not in nums:
+            output.append(i)
+    return output
+
+print(findDisappearedNumbersBF([1,1,1]))
