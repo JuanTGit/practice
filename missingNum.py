b26978852f2f23a884a90bb9d4d957d559e26a16
Juan <j.tejeda46@yahoo.com>
2024-01-20 00:23:02 -0600

    Leetcode 58. Length of last word

--- missingNum.py
+++ missingNum.py
@@ -5,3 +5,23 @@ def missing_number(nums):
             return num
 
 print(missing_number([3, 0, 1, 4, 6, 2]))
+
+
+nums = {
+    0: 1,
+    1: 1,
+    2: 1
+}
+
+nums.pop(0)
+
+if 1 in nums.keys():
+    print('good')
+
+
+strs = ['flowers', 'flow', 'flight']
+res = ''
+
+smallest = len(min(strs))
+        
+print(smallest)
