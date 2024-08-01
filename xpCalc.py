ba48186eccfb04f7d033b9b2ed54c2e76c5a27bc
Juan <j.tejeda46@yahoo.com>
2024-08-01 10:08:02 -0500

    Added math to script

--- xpCalc.py
+++ xpCalc.py
@@ -1,12 +1,13 @@
-
+import math
 
 def calculate_actions(curr_xp, xp_per_action):
 	max_lvl = 13034431
-	xp_to_max = max_lvl - curr_xp
+	xp_to_max = math.ceil(max_lvl - curr_xp)
 
-	actions_remaining = xp_to_max / xp_per_action
+	print(f'xp remaining to max: {xp_to_max}')
+	actions_remaining = math.ceil(xp_to_max / xp_per_action)
 
-	return actions_remaining
+	return f'{actions_remaining} actions remain'
 
 
 print(calculate_actions(5089700, 32.8))
