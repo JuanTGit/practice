28317765c7ed6741c6ae7c6a5d81533543809b4e
Juan <j.tejeda46@yahoo.com>
2025-02-05 02:31:32 -0600

    transpose matrix

---
+++ tposeMatrix.py
@@ -0,0 +1,20 @@
+matrix = [
+    [0, -1, -2, -3],
+    [4, 5, 6, 7],
+    [2, 3, -2, -3],
+    [42, 100, 30, -42]
+]
+
+
+def transposeMatrix(matrix):
+    
+	res = []
+
+	for row in matrix:
+		# print(row)
+		for col in row:
+			print(col)
+		print('----------')
+
+
+print(transposeMatrix(matrix))
