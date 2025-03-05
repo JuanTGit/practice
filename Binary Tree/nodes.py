c20e95bde56ea73602d4af4475e9c14ebd483ab6
Juan <j.tejeda46@yahoo.com>
2025-03-05 15:17:10 -0600

    Tree practice

---
+++ Binary Tree/nodes.py
@@ -0,0 +1,33 @@
+from tree import TreeNode
+
+root = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+node7 = TreeNode(7)
+node8 = TreeNode(8)
+node9 = TreeNode(9)
+
+
+
+root.left = node2
+root.right = node3
+node2.left = node4
+node2.right = node5
+node3.left = node6
+node3.right = node7
+node4.left = node8
+node4.right = node9
+
+
+def searchRight(root):
+	current = root
+
+	while current:
+		print(current.val)
+		current = current.left
+
+
+searchRight(root)
