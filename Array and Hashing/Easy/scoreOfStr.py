b11396cc8b6d751ec3254e0efa83f2ba6993e892
Juan <j.tejeda46@yahoo.com>
2025-03-30 11:23:36 -0500

    score of a string

--- Array and Hashing/Easy/scoreOfStr.py
+++ Array and Hashing/Easy/scoreOfStr.py
@@ -32,3 +32,14 @@
 """
 https://leetcode.com/problems/score-of-a-string/description/
 """
+
+def scoreOfString(s):
+	res = 0
+
+	for i in range(len(s) - 1):
+		res += abs(ord(s[i]) - ord(s[i + 1]))
+	return res
+
+
+scoreOfString('hello')
+
