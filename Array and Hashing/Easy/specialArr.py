ace858af052606b0e7c5c9e1c0c8d12037220e94
Juan <j.tejeda46@yahoo.com>
2025-06-12 11:34:19 -0500

    3151. Special Array I

--- Array and Hashing/Easy/specialArr.py
+++ Array and Hashing/Easy/specialArr.py
@@ -8,8 +8,6 @@
 
 # You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
 
- 
-
 # Example 1:
 # Input: nums = [1]
 # Output: true
@@ -23,7 +21,7 @@
 # There is only two pairs: (2,1) and (1,4), and both of them contain numbers with different parity. So the answer is true.
 
 # Example 3:
-# Input: nums = [4,3,1,6]
+# Input: nums = [4,3,2,5,6]
 # Output: false
 # Explanation:
 # nums[1] and nums[2] are both odd. So the answer is false.
@@ -33,3 +31,22 @@
 
 # 1 <= nums.length <= 100
 # 1 <= nums[i] <= 100
+
+
+def isArraySpecial(nums):
+	if len(nums) <= 1: return True
+	isEven = False
+	if nums[0] % 2 == 0: isEven = True
+
+	for i in range(1, len(nums)):
+		if nums[i] % 2 == 0 and isEven:
+			return False
+		elif nums[i] % 2 != 0 and not isEven:
+			return False
+		else:
+			isEven = not isEven
+
+	return True
+		
+
+print(isArraySpecial([4,3,2,5,6,6]))
