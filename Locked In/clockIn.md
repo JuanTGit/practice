373891a5c9e2e0621a11b61777cf43ac0f45cb70
Juan Tejeda <j.tejeda46@yahoo.com>
2025-12-25 01:21:53 -0600

    binary search

---
+++ Locked In/clockIn.md
@@ -0,0 +1,5 @@
+
+# Lock In and Clock in
+
+
+
--- binary.py
+++ binary.py
@@ -16,6 +16,6 @@ def binarySearch(array, target):
 
 
 
-search = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+search = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 print(binarySearch(search, 13))
