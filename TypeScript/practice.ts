f5d23b49a841485c1931c2f1815001d927dc5c5a
Juan Tejeda <j.tejeda46@yahoo.com>
2026-04-15 22:03:48 -0500

    async ts practice

--- TypeScript/practice.ts
+++ TypeScript/practice.ts
@@ -4,19 +4,19 @@ Type Annotations
 JS = const itemName: "plank";
 TS = const itemName: string = "plank";
 */
-
+console.log("===============Type Annotations===============")
 const itemName: string = "Dragon bones";
-const itemID: number = 537;
+// const itemID: number = 4151;
 const isMembers: boolean = true;
 
 console.log(itemName);
-console.log(itemID);
+// console.log(itemID);
 console.log(isMembers);
 
 /* 
 Interfaces
 */
-
+console.log("===============Interfaces===============")
 interface Item {
 	id: number;
 	name: string;
@@ -30,3 +30,49 @@ const dragonBones: Item = {
 }
 
 console.log(dragonBones)
+
+/* 
+Typed Functions
+*/
+console.log("===============Typed Functions===============")
+// Function that returns a value
+function formatItemName(name: string): string {
+	return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+}
+// Void = do something not give something back. (side effect)
+function logPrice(price: number): void {
+	console.log(`Price: ${price.toLocaleString()} gp`);
+}
+
+console.log(formatItemName("dragon bones"));
+logPrice(4200);
+
+/* 
+Async Functions
+*/
+console.log("===============Async Functions with Types===============")
+
+interface PriceData {
+	high: number;
+	hightTime: number;
+	low: number;
+	lowTime: number;
+}
+
+async function getItemPrice(itemId: number): Promise<PriceData> {
+	const response = await fetch(
+		`https://prices.runescape.wiki/api/v1/osrs/latest?id=${itemId}`
+	);
+
+	if (!response.ok) {
+		throw new Error(`Failed to fetch price for item ${itemId}`);
+	}
+
+	const data = await response.json();
+	return data.data[itemId]
+}
+
+getItemPrice(4151).then(price => {
+	// console.log(price);
+	console.log(`${price.high.toLocaleString()} gp`);
+})
